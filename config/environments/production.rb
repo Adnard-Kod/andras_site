aeddc5d11e18bcd964114d17845d9836f63faa88
Adnard-kod <andra.lally@gmail.com>
2015-07-13 23:04:04 -0500

    upadte asw var

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   config.paperclip_defaults = {
     :storage => :s3,
+    :s3_host_name => 's3-us-west-2.amazonaws.com',
     s3_credentials: "#{Rails.root}/config/s3.yml"
   }
 end
